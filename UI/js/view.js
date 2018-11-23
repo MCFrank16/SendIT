f8c790abcb9bc8f07c785e29d0e63f50d1e74c5d
Frank C. Mutabazi <mecfrank16@gmail.com>
2018-11-23 08:06:34 +0200

    bg-update-ui-#163024216

--- UI/js/view.js
+++ UI/js/view.js
@@ -1,25 +1,25 @@
 // Get the modal
-var modal = document.getElementById('ViewM');
+const modall = document.getElementById('ViewM');
 
 // Get the button that opens the modal
-var btn = document.getElementById("btn");
+const btnn = document.getElementById('btn1');
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const spann = document.getElementsByClassName('close')[0];
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+// When the user clicks on the button, open the modal
+btn1.onclick = function () {
+  modall.style.display = 'block';
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+spann.onclick = function () {
+  modall.style.display = 'none';
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+window.onclick = function (event) {
+  if (event.target == modall) {
+    modall.style.display = 'none';
+  }
+};
